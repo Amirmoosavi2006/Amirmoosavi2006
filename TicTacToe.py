a18780665b2b3965f065d3798a0f7f225cfb8c57
Amir <amirmoosavi2006@gmail.com>
2022-12-22 20:17:57 +0330

    My New Codes

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,7 +1,10 @@
+import os
+
 player = []
 computer = []
 choices = [1,2,3,4,5,6,7,8,9]
 allChoices = [1,2,3,4,5,6,7,8,9]
+winner = ((1,2,3),(4,5,6),(7,8,9),(1,4,7),(2,5,8),(3,6,9),(1,5,9),(3,5,7))
 sign = ['X']
 
 def printcross():
@@ -15,10 +18,16 @@ def printcross():
         if i % 3 == 0:
             print('\n\n')
 
+def printChoices():
+    print('Remain choices:')
+    for i in choices:
+        if not i in ('X','O'):
+            print(i,end='\t')
+
 def dogame():
     while True:
-        print('Remain choices:', choices)
-        inputNumber = input('Enter your Choice: ')
+        printChoices()
+        inputNumber = input('\nEnter your Choice: ')
         if inputNumber.isnumeric():
             inputNumber = int(inputNumber)
             if inputNumber in choices:
@@ -33,13 +42,43 @@ def dogame():
                 break
         print('Wrong choice!\nTry again...')
 
-print ('Welcome to Tic-Tac-Toe game...\n')
-printcross()
-print('*'*10,'\n',sign,'\n','*'*10)
-dogame()
-print('*'*10,'\n',sign,'*'*10)
-printcross()
-dogame()
-printcross()
-dogame()
-printcross()
+def sub_lists (l):
+    lists = [[]]
+    for i in range(len(l)+1):
+        for j in range(i):
+            if len(l[j:i]) == 3:
+                lists.append(l[j:i])
+    return lists
+
+def checkWin(l):
+    for i in winner:
+        for j in l:
+            if sorted(j) == sorted(i):
+                return True
+    return False
+
+while True:
+    os.system('cls')
+    print ('Welcome to Tic-Tac-Toe game...\n')
+    printcross()
+    dogame()
+    if checkWin(sub_lists(player)) is True:
+        os.system('cls')
+        printcross()
+        print('You win...')
+        break
+    elif checkWin(sub_lists(computer)) is True:
+        os.system('cls')
+        printcross()
+        print('Computer win...')
+        break
+    else:
+        checkList = []
+        for i in choices:
+            if not i in ('X', 'O'):
+                checkList.append(i)
+        if len(checkList) < 1:
+            os.system('cls')
+            printcross()
+            print("It's tie...")
+            break
